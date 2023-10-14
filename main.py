12627a030db50d7019d405ee9249c37922c21e9e
kangapi <pourlessiteswebnuls@gmail.com>
2023-10-14 17:13:03 +0200

    Add colors

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from shutil import move
 import mimetypes
 import typer
 from rich.progress import track
+from rich.console import Console
+console = Console()
 
 def main(from_folder: str = typer.Argument(help="The folder where your files are"),
          to_folder: str = typer.Argument(help="Where you want to move your files"),
          device_name: str = typer.Argument(help="The name of the device")):
 
+    console.print("[blue]Looks like a link")
+
     total = 0
+    good_total = 0
+    bad_total = 0
+
     for value in track(range(100), description="Processing..."):
 
         goodMimeTypes = ["image/jpeg", "image/png", "image/heic", "video/mp4", "video/quicktime", "image/x-adobe-dng",
@@ -33,11 +40,25 @@ def main(from_folder: str = typer.Argument(help="The folder where your files are
                 new_file_path = os.path.join(day_folder, fileDateStr + "_" + device_name + "_" + file)
                 move(file_path, new_file_path)
 
+                good_total += 1
+
             else:
-                print(f"Skipping {file} because it is not in the list of good mime types")
+                # Move the file to a folder called "bad"
+                bad_folder = os.path.join(to_folder, "bad")
+                if not os.path.exists(bad_folder):
+                    os.makedirs(bad_folder)
+                new_file_path = os.path.join(bad_folder, file)
+                move(from_folder + "/" + file, new_file_path)
+                # Log into a file
+                with open(to_folder + "/-log.txt", "a") as log_file:
+                    log_file.write(file + "\n")
+
+                bad_total += 1
 
             total += 1
     print(f"Processed {total} files.")
+    console.print(f"[green]{good_total}[/green] videos or photos have correctly been moved.")
+    console.print(f"[red]{bad_total}[/red] files unsupported (see log file (-log.txt) and \"bad\" folder in the destination directory).")
 
 if __name__ == "__main__":
     typer.run(main)
