3d47d4f490515674d2f71b6073a43a351e232ce0
kangapi <pourlessiteswebnuls@gmail.com>
2023-10-14 19:22:43 +0200

    Fix the progress bar

--- main.py
+++ main.py
@@ -3,62 +3,63 @@ import time
 from shutil import move
 import mimetypes
 import typer
-from rich.progress import track
+from rich.progress import Progress
 from rich.console import Console
+
 console = Console()
+
 
 def main(from_folder: str = typer.Argument(help="The folder where your files are"),
          to_folder: str = typer.Argument(help="Where you want to move your files"),
          device_name: str = typer.Argument(help="The name of the device")):
+    goodMimeTypes = ["image/jpeg", "image/png", "image/heic", "video/mp4", "video/quicktime", "image/x-adobe-dng",
+                     "image/gif", "video/m4v", "video/x-m4v"]
 
-    console.print("[blue]Looks like a link")
+    file_list = os.listdir(from_folder)
+    total = len(file_list)
 
-    total = 0
-    good_total = 0
-    bad_total = 0
+    with Progress() as progress:
+        task = progress.add_task("[cyan]Processing files...", total=total)
 
-    for value in track(range(100), description="Processing..."):
+        good_total = 0
+        bad_total = 0
 
-        goodMimeTypes = ["image/jpeg", "image/png", "image/heic", "video/mp4", "video/quicktime", "image/x-adobe-dng",
-                         "image/gif", "video/m4v", "video/x-m4v"]
-
-        for file in os.listdir(from_folder):
-
+        for file in file_list:
             if mimetypes.guess_type(file)[0] in goodMimeTypes:
-
                 file_path = os.path.join(from_folder, file)
                 fileDate = os.path.getmtime(file_path)
                 folderDateStr = time.strftime("%Y%m%d", time.localtime(fileDate))
-                fileDateStr = time.strftime("%Y%m%d%H%M%S", time.localtime(fileDate))
+                fileDateStr = time.strftime("%Y%m%d%-H%M%S", time.localtime(fileDate))
 
-                # Crée un répertoire pour la date correspondante s'il n'existe pas
                 day_folder = os.path.join(to_folder, folderDateStr)
                 if not os.path.exists(day_folder):
                     os.makedirs(day_folder)
 
-                # Déplace le fichier dans le répertoire correspondant en le renommant
                 new_file_path = os.path.join(day_folder, fileDateStr + "_" + device_name + "_" + file)
                 move(file_path, new_file_path)
 
-                good_total += 1
+                # Wait 0.1s to avoid same timestamp
+                time.sleep(0.001)
 
+                good_total += 1
             else:
-                # Move the file to a folder called "bad"
-                bad_folder = os.path.join(to_folder, "bad")
+                bad_folder = os.path.join(to_folder, "- Bad")
                 if not os.path.exists(bad_folder):
                     os.makedirs(bad_folder)
                 new_file_path = os.path.join(bad_folder, file)
-                move(from_folder + "/" + file, new_file_path)
-                # Log into a file
-                with open(to_folder + "/-log.txt", "a") as log_file:
-                    log_file.write(file + "\n")
+                move(os.path.join(from_folder, file), new_file_path)
 
+                with open(os.path.join(to_folder, "- log.txt"), "a") as log_file:
+                    log_file.write(file + "\n")
                 bad_total += 1
 
-            total += 1
+            progress.update(task, completed=good_total + bad_total)
+
     print(f"Processed {total} files.")
     console.print(f"[green]{good_total}[/green] videos or photos have correctly been moved.")
-    console.print(f"[red]{bad_total}[/red] files unsupported (see log file (-log.txt) and \"bad\" folder in the destination directory).")
+    console.print(
+        f"[red]{bad_total}[/red] files unsupported, see log file [red]\"- log.txt\"[/red] and [red]\"- Bad\"[/red] folder in the destination directory.")
+
 
 if __name__ == "__main__":
     typer.run(main)
